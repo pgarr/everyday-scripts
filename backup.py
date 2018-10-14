4213aeaa9e46f3bcac5007e6cb7066054035caa5
pgarr <garlej.p@gmail.com>
2018-10-14 12:23:20 +0200

    directory validation small refactoring

--- backup.py
+++ backup.py
@@ -4,11 +4,19 @@ import shutil
 
 BACKUP_THIS_DIRS = ("D:\pgarr",)
 
-main_dir = input("Please enter main directory: ") # TODO: walidacja podanej ścieżki
-now = datetime.datetime.today().strftime('%Y-%m-%d')
-directory = os.path.join(main_dir, "BACKUP", now)
+
+def backup_directory(main_dir):
+    now = datetime.datetime.today().strftime('%Y-%m-%d')
+    return os.path.join(main_dir, "BACKUP", now)
+
+
+main_dir = input("Please enter main directory:")
+directory = backup_directory(main_dir)
 
 print(directory)
 
-for dir_ in BACKUP_THIS_DIRS:
-    shutil.copytree(dir_, directory)
+try:
+    for dir_ in BACKUP_THIS_DIRS:
+        shutil.copytree(dir_, directory)
+except FileNotFoundError as e:
+    print(e)
