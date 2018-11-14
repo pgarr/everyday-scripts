78e6418a430e67646aff62fc5b501fd8e0f00234
pgarr <garlej.p@gmail.com>
2018-11-14 22:45:58 +0100

    basic zip compression with flat folder structure (all files saved in one folder)

--- backup.py
+++ backup.py
@@ -1,22 +1,44 @@
 import datetime
 import os
 import shutil
+import zipfile
 
-BACKUP_THIS_DIRS = ("D:\pgarr",)
+# BACKUP_THIS_DIRS = ("D:\pgarr",)
+BACKUP_THIS_DIRS = ("D:\pgarr\Documents\praca",)
 
 
-def backup_directory(main_dir):
+def backup_directory(this_dir):
     now = datetime.datetime.today().strftime('%Y-%m-%d')
-    return os.path.join(main_dir, "BACKUP", now)
+    back_dir = os.path.join(this_dir, "BACKUP", now)
+    if not os.path.exists(back_dir):
+        os.makedirs(back_dir)
+    return back_dir
 
 
-main_dir = input("Please enter main directory:")
+def zip_this_dir(path, zipped_file):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zipped_file.write(os.path.join(root, file), os.path.basename(os.path.normpath(path)) + "\\" + file)
+
+
+def make_zip(folder_to_zip, destination, zip_file_name):
+    zipped_file = zipfile.ZipFile(os.path.join(destination, zip_file_name), 'w', zipfile.ZIP_DEFLATED)
+    zip_this_dir(folder_to_zip, zipped_file)
+    zipped_file.close()
+
+
+main_dir = input("Please enter backups main directory:")
 directory = backup_directory(main_dir)
 
-print(directory)
+for dir_ in BACKUP_THIS_DIRS:
+    make_zip(dir_, directory, 'zipfile.zip')
 
-try:
-    for dir_ in BACKUP_THIS_DIRS:
-        shutil.copytree(dir_, directory)
-except FileNotFoundError as e:
-    print(e)
+    #
+    # try:
+    #     for dir_ in BACKUP_THIS_DIRS:
+    #         zip_path = os.path.join(directory, 'Python.zip')
+    #         zipf = zipfile.ZipFile('Python.zip', 'w', zipfile.ZIP_DEFLATED)
+    #         zipdir(dir_, zipf)
+    #         zipf.close()
+    # except FileNotFoundError as e:
+    #     print(e)
