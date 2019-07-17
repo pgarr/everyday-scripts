328ae1f9f5cb2a33e3690634369b49899bd29d36
pgarr <garlej.p@gmail.com>
2019-07-17 21:59:14 +0200

    logging, some improvements to code

--- backup.py
+++ backup.py
@@ -1,8 +1,13 @@
 import datetime
+import logging
 import os
 import zipfile
 
-BACKUP_THIS_DIRS = ("D:\pgarr",)
+BACKUP_THIS_DIRS = (
+    "D:\pgarr\Documents", "D:\pgarr\instalki", "D:\pgarr\Music", "D:\pgarr\Pictures", "D:\pgarr\pobrane",
+    "D:\pgarr\Videos")
+
+
 # BACKUP_THIS_DIRS = ("D:\pgarr\Documents\praca",)  # for tests
 
 
@@ -25,15 +30,22 @@ def zip_this_dir(path, zipped_file):
         for file in files:
             actual_file_path = root + '/' + file
             zipped_file_path = prefix + file
-            zipped_file.write(actual_file_path, zipped_file_path)
+            try:
+                zipped_file.write(actual_file_path, zipped_file_path)
+            except Exception as e:
+                logging.warning("Unable to backup file '%s'" % actual_file_path)
+                logging.warning("Reason: %s, %s" % (e.__class__.__name__, e))
+                continue
+            logging.info("File '%s' stored" % actual_file_path)
 
 
 def make_zip(folder_to_zip, destination, zip_file_name):
-    zipped_file = zipfile.ZipFile(os.path.join(destination, zip_file_name), 'w', zipfile.ZIP_DEFLATED)
-    zip_this_dir(folder_to_zip, zipped_file)
-    zipped_file.close()
+    logging.info("Storing folder: %s" % folder_to_zip)
+    with zipfile.ZipFile(os.path.join(destination, zip_file_name), 'w', zipfile.ZIP_DEFLATED) as zipped_file:
+        zip_this_dir(folder_to_zip, zipped_file)
 
 
+logging.basicConfig(level=logging.INFO)  # TODO: lepsze logi, data, godzina, itp.
 main_dir = input("Please enter backups main directory:")
 directory = backup_directory(main_dir)
 
